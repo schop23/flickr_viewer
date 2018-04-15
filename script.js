157b70e8bbdeba068e345a3b04448b1041a7995a
Shelley Schoppert <schop23@gmail.com>
2018-04-15 17:56:25 -0400

    searchBox

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ jQuery(document).ready(function ($) {
   var searchTag
   $('#searchButton').click(function (evt) {
     evt.preventDefault()
+    searchTag = ' '
+    if ($('#searchBox').val() === ' ') {
+      alert('Please enter one or more tags to proceed.')
+    } else {
+      searchTag = $('searchBox').val()
+    }
   })
 })
